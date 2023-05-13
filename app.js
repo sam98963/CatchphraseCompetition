7c91735b049c16da3e6da3fa589246ad1bcab80d
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-05-13 09:42:31 +0100

    completed all functions and route handlers

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
+
+
+
 import express from "express";
 
 const app = express();
 const port = 3000;
 
+import {
+  getUsers,
+  getUserByID,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
+} from "./users.js";
+
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.json({
-    status: true,
-    payload: "This route works!",
-  });
+// app.use("/", (req, res) => {
+//   res.json({
+//     status: true,
+//     payload: "This route works!",
+//   });
+// });
+
+// .get for all users { success: true, payload: array of user objects }
+app.get("/api/users", async (req, res) => {
+  const allUsers = await getUsers();
+  res.send({ success: true, payload: allUsers });
 });
+
+// .get for get user by id { success: true, payload: user object }
+app.get("/api/users/:id", async (req, res) => {
+  const userID = req.params.id;
+  const oneUser = await getUserByID(userID);
+  res.send({ success: true, payload: oneUser });
+});
+
+// .post for new user (generate i.d within helper function using uuidv4) { success: true, payload: newly created user object }
+app.post("/api/users", async (req, res) => {
+  const user = req.body;
+  const newUser = await createUser(user);
+  res.send({ success: true, payload: newUser });
+});
+
+// .patch for updateUserById .body and .id to be passed into helper function. { success: true, payload: new user object (after replacement) }
+app.patch("/api/users/:id", async (req, res) => {
+  const userID = req.params.id;
+  const user = req.body;
+  const newUser = await updateUserByID(userID, user);
+  res.send({ success: true, payload: newUser });
+});
+
+// .delete for delete user by Id { success: true, payload: deleted user }
+app.delete("/api/users/:id", async (req, res) => {
+  const userID = req.params.id;
+  const deletedUser = await deleteUserByID(userID);
+  res.send({ success: true, payload: deletedUser });
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- users.js
+++ users.js
@@ -1,14 +1,74 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
-
 const fileName = "users.json";
-
-export async function getUsers() {}
-
-export async function getUserByID(id) {}
-
-export async function createUser(newUser) {}
-
-export async function updateUserByID(id, updatedUser) {}
-
-export async function deleteUserByID(id) {}
+export async function getUsers() {
+  const response = await fs.readFile(fileName);
+  const data = JSON.parse(response);
+  return data;
+}
+// Read json
+// parse
+// return data
+export async function getUserByID(id) {
+  const response = await fs.readFile(fileName);
+  const data = JSON.parse(response);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      return data[i];
+    } else {
+    }
+  }
+  return "error 401 - Not Found";
+}
+// Read json
+// parse
+// for loop and match id - else throw error
+// return data[i]
+export async function createUser(newUser) {
+  const response = await fs.readFile(fileName);
+  const data = JSON.parse(response);
+  const userObject = { id: uuidv4(), ...newUser };
+  data.push(userObject);
+  await fs.writeFile(fileName, JSON.stringify(data), "utf-8");
+  return userObject;
+}
+// Read json
+// parse
+// create data object {id: uuidv4, ...newUser}
+// stringify
+// write to file (destination, file to write, utf-8)
+// return new user
+export async function updateUserByID(id, updatedUser) {
+  const response = await fs.readFile(fileName);
+  const data = JSON.parse(response);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      data[i] = { id: data[i].id, ...updatedUser };
+      await fs.writeFile(fileName, JSON.stringify(data), "utf-8");
+      return data[i];
+    } else {
+    }
+  }
+  return "error 401 - Not Found";
+}
+// read
+// parse
+// for loop
+// update data to be updated data (data[i] = {id:id, ...updatedUser})
+// stringify
+// write
+// return updated user
+export async function deleteUserByID(id) {
+  const response = await fs.readFile(fileName);
+  const data = JSON.parse(response);
+  // console.log(data)
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      let deleted = data.splice(i, 1);
+      await fs.writeFile(fileName, JSON.stringify(data), "utf-8");
+      console.log(deleted);
+      return deleted[0];
+    }
+  }
+}
+// deleteUserByID("ca1af314-357f-4c09-b7b3-9572af6ec13d")
